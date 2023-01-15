d70d4aa89e6b77007f04e917f8d5ba2a66f4f1d1
Fitz Ignacio <ignacio.fitz@gmail.com>
2023-01-14 21:30:17 -0800

    commit tree c2d64bd2fb9e0858ea68c79fdcadccfb76a42ccf

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat (name) {
+    const copyCat = [...cats, name]
+    return copyCat;
+}
+
+function prependCat (name) {
+    const copyCat = [name, ...cats]
+    return copyCat;
+}
+
+function removeLastCat() {
+    const copyCat = cats.slice(0, cats.length -1)
+    return copyCat;
+}
+
+function removeFirstCat() {
+    const copyCat = cats.slice(1)
+    return copyCat;
+}
